f593ac9166ff0821b83f980e2d7eb45c962026cf
mg <michel.gendre@softcom.pro>
2018-05-11 12:40:16 +0200

    Prepare for AWS

--- src/main/kotlin/org/rpg/inventory/Application.kt
+++ src/main/kotlin/org/rpg/inventory/Application.kt
@@ -1,12 +1,17 @@
 package org.rpg.inventory
 
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
   SpringApplication.run(Application::class.java, *args)
 }
-
